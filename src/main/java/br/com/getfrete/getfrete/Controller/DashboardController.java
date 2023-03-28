85a600731d3c4e7790d2c777aac7056fc3505413
Lucas Lima <lucaslavey@outlook.com>
2023-03-27 23:55:21 -0300

    Add new scenes

--- src/main/java/br/com/getfrete/getfrete/Controller/DashboardController.java
+++ src/main/java/br/com/getfrete/getfrete/Controller/DashboardController.java
@@ -1,27 +1,35 @@
 package br.com.getfrete.getfrete.Controller;
 
+import br.com.getfrete.getfrete.Dao.Conexao;
 import br.com.getfrete.getfrete.Model.LoginModel;
 import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.*;
 import java.util.ResourceBundle;
 
-public class DashboardController {
+public class DashboardController implements Initializable {
     @FXML
     private Label txtName;
     LoginModel data = LoginModel.getInstance();
-    public void initialize() {
-        txtName.setText(data.getUserName().toLowerCase());
-    }
+
     @FXML
     private JFXButton btnBudget;
 
@@ -40,8 +48,37 @@ public class DashboardController {
 
     @FXML
     private Button btnApagar;
+    @FXML
+    private Label tvNomeUser;
 
+    @FXML
+    private ImageView imgEdit;
+    @FXML
+    private Button btnExcluirConta;
 
+    @FXML
+    private Button btnSalvarEdit;
+
+    @FXML
+    private TextField txtCnhEdit;
+
+    @FXML
+    private TextField txtCpf;
+
+    @FXML
+    private TextField txtDataEdit;
+
+    @FXML
+    private TextField txtEmailEdit;
+
+    @FXML
+    private TextField txtNomeEdit;
+
+    @FXML
+    private PasswordField txtSenhaEdit;
+
+    @FXML
+    private TextField txtTelefoneEdit;
     @FXML
     void Apagar(ActionEvent event) {
 
@@ -56,13 +93,15 @@ public class DashboardController {
     }
 
     @FXML
-    void btnMyJobs(ActionEvent event) {
-
+    void btnMyJobs(ActionEvent event) throws IOException {
+        AnchorPane view = (AnchorPane)FXMLLoader.load(getClass().getResource("/br/com/getfrete/getfrete/myJobs.fxml"));
+        borderpane.setCenter(view);
     }
 
     @FXML
-    void btnMyTrucks(ActionEvent event) {
-
+    void btnMyTrucks(ActionEvent event) throws IOException {
+        AnchorPane view = (AnchorPane)FXMLLoader.load(getClass().getResource("/br/com/getfrete/getfrete/myTrucks.fxml"));
+        borderpane.setCenter(view);
     }
 
     @FXML
@@ -70,6 +109,52 @@ public class DashboardController {
         AnchorPane view = (AnchorPane)FXMLLoader.load(getClass().getResource("/br/com/getfrete/getfrete/profile.fxml"));
         borderpane.setCenter(view);
     }
+    @FXML
+    void btnEdit(MouseEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/br/com/getfrete/getfrete/profileEdit.fxml"));
+        Scene scene = new Scene(root);
+        Stage primaryStage = new Stage();
+        primaryStage.setTitle("Edite seu perfil");
+        primaryStage.setScene(scene);
+        primaryStage.initModality(Modality.WINDOW_MODAL);
+        primaryStage.showAndWait();
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Label tvNomeUser = new Label();
+        String user = data.getUserName();
+        Conexao connection = new Conexao();
+        try {
+            PreparedStatement stmt = connection.getConnection().prepareStatement("SELECT * FROM caminhoneiro where cpf = ?");
+            stmt.setInt(1, Integer.parseInt(user));
+            ResultSet rs = stmt.executeQuery();
+
+            String nome = null;
+
+            if(rs.next()){
+                nome = rs.getString("nome");
+
+                tvNomeUser.setText(nome);
+
+            }
+            stmt.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    @FXML
+    void ExcluirConta(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SalvarEdit(ActionEvent event) {
+
+    }
+
 
 }
 
